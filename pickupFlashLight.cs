f75d56ef788e12a4aa1e5f36e72362212f51c76a
Mnuss06 <133200606+Mnuss06@users.noreply.github.com>
2023-12-21 08:45:24 -0700

    Update pickupFlashLight.cs

--- pickupFlashLight.cs
+++ pickupFlashLight.cs
@@ -64,7 +64,7 @@ public class pickupFlashlight : MonoBehaviour
 
                 interactable = false;
 
-                //pickup.Play();
+        
 
                 flashlight_hand.SetActive(true);
 
